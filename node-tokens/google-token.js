073700d733fe8dee5818287e58d50f242eb8bd20
Youssef Swailem <youssefswailem@gmail.com>
2017-07-30 12:49:38 +0200

    Added clientID check

--- node-tokens/google-token.js
+++ node-tokens/google-token.js
@@ -5,11 +5,15 @@ var validateClientToken = function(token, callback){
 
   var url  = 'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=' + token;
 
+  //get your clientID for your app from the google devs console
+  var clientID = 'your-client-id-here';
+
   request(url, function(error, response, body){
 
-    //a token should only be valid if the JSON object does not have
-    //an error field
-    var isValid = JSON.parse(body).error_description === undefined;
+    var response = JSON.parse(body);
+
+    //check that there is no error and that the clientID is the same
+    var isValid = response.error_description === undefined && response.aud === clientID;
 
     if(callback !== undefined){
           callback(isValid);
